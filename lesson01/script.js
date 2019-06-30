9c087af8e6e46c08cba684efdc0b4e19c89da141
Andrey Kuprienko <lupp@ukr.net>
2019-06-30 19:50:42 +0200

    Создал Lesson1

---
+++ lesson01/script.js
@@ -0,0 +1,11 @@
+let money = 10;
+let deposit = 20;
+let income;
+let addExpenses;
+let mission;
+let period;
+console.log('money', money);
+console.log('deposit', deposit);
+console.log('sum',deposit + money);
+
+alert("Не придумал что сюда написать)))");
