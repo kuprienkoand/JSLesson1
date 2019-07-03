5e613c9df856f20c4ad4f5aa8782e18fc9cba46c
Andrey Kuprienko <lupp@ukr.net>
2019-07-03 18:25:41 +0200

    внес правки в урок 3

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt('Ваш месячный доход?');
+let money = +prompt('Ваш месячный доход?');
 console.log('1) money:', typeof money);
 
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
@@ -9,14 +9,10 @@ console.log('2) addExpenses (возможные расходы):', addExpenses.s
 let deposit  = confirm('Есть ли у вас депозит в банке?');
 console.log('3) deposit (Есть ли у вас депозит в банке?):', typeof deposit);
 
-let question = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    question2 = prompt('Во сколько это обойдется?'),
-    question3 = question,
-    question4 = question2;
-/* console.log('question:', question);
-console.log('question2:', question2);
-console.log('question3:', question3);
-console.log('question4:', question4); */
+let question = +prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    question2 = +prompt('Во сколько это обойдется?'),
+    question3 = +prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    question4 = +prompt('Во сколько это обойдется?');
 
 let budgetMonth = (money - question);
 console.log('6) доход за месяц:', budgetMonth);
@@ -24,17 +20,17 @@ console.log('6) доход за месяц:', budgetMonth);
 let mission = (question2 / budgetMonth);
 console.log('7) за сколько месяцев будет достигнута цель:', parseInt(mission), 'месяцев');
 
+
 let budgetDay = (budgetMonth / 30);
 console.log('8) budgetDay:', Math.floor(budgetDay));
 
 let n = budgetDay;
 if (n > 800) {
   console.log('Высокий уровень дохода');
-} else if (800 >= n >= 300) {
+} else if (n <=800 && n > 300) {
   console.log('Средний уровень дохода');
-} else if (300 > n >= 0) {
+} else if (n <=300 && n >= 0) {
   console.log('Низкий уровень дохода');
 } else if (n < 0) {
   console.log('Что то пошло не так)))');
 };
-
