af716846fe80f5e1d5a59029d8dbdea860c240f3
Andrey Kuprienko <lupp@ukr.net>
2019-07-22 19:15:32 +0200

    Внес правки

--- you-dont-know-js/you-dont-know-js/script.js
+++ you-dont-know-js/you-dont-know-js/script.js
@@ -20,8 +20,10 @@ body[0].removeChild(adv);
 
 myUl[0].insertBefore(myLi[6], myLi[4]);
 myUl[0].insertBefore(myLi[8], myLi[4]);
+myUl[0].insertBefore(myLi[2], myLi[10]);
 myUl[5].insertBefore(myLi[55], myLi[48]);
 myUl[5].insertBefore(myLi[48], myLi[51]);
+myUl[5].insertBefore(myLi[51], myLi[54]);
 
 let newElem = document.createElement('li');
 newElem.textContent = 'Глава 8: За пределами ES6';
